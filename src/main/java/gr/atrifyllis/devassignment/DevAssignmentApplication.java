fab4ba0706388525c7401699ea6614de271ff0f2
Alexandros Trifyllis <a.trifyllis@gmail.com>
2019-09-26 10:08:30 +0300

    move CommandLineRunner to own class so we can annotate it with @Profile

--- src/main/java/gr/atrifyllis/devassignment/DevAssignmentApplication.java
+++ src/main/java/gr/atrifyllis/devassignment/DevAssignmentApplication.java
@@ -2,40 +2,44 @@ package gr.atrifyllis.devassignment;
 
 import gr.atrifyllis.devassignment.order.OrderRepository;
 import gr.atrifyllis.devassignment.order.OrderSampleCreator;
-import gr.atrifyllis.devassignment.order.PlacedOrder;
-import gr.atrifyllis.devassignment.product.Product;
 import gr.atrifyllis.devassignment.product.ProductRepository;
 import gr.atrifyllis.devassignment.product.ProductSampleCreator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @SpringBootApplication
-public class DevAssignmentApplication implements CommandLineRunner {
-
-    private final ProductRepository productRepository;
-    private final OrderRepository orderRepository;
-
-    public DevAssignmentApplication(ProductRepository productRepository, OrderRepository orderRepository) {
-        this.productRepository = productRepository;
-        this.orderRepository = orderRepository;
-    }
+public class DevAssignmentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevAssignmentApplication.class, args);
     }
 
+}
+
+@Profile("!docker")
+@Component
+// do not initialize products when running in docker
+class DataInitializer implements CommandLineRunner {
+
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+
+    DataInitializer(ProductRepository productRepository, OrderRepository orderRepository) {
+        this.productRepository = productRepository;
+        this.orderRepository = orderRepository;
+    }
+
     @Override
     @Transactional
+
     // this won't run correctly if it not annotated as transactional.
     public void run(String... args) {
         productRepository.saveAll(ProductSampleCreator.getTwoProducts());
-        List<Product> persistedProducts = productRepository.findAll();
-        PlacedOrder order = OrderSampleCreator.getOrder(persistedProducts);
-        orderRepository.save(order);
+        orderRepository.save(OrderSampleCreator.getOrder(productRepository.findAll()));
     }
 }
 
